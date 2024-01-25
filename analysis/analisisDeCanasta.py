92cab659f1db587a2b89617e543d1abf1eaa1a80
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-01-24 20:27:38 -0500

    nt3 se crea la base de datos con python con su respetiva creacion de tabla

--- analysis/analisisDeCanasta.py
+++ analysis/analisisDeCanasta.py
@@ -1,17 +1,18 @@
-#1. Importar el paquete o paquetes con los que voy analizar la informacion
+#1. Importar el paquete o paquetes con los que voy a analisar la informacion
 
-import pandas as pd
+import pandas as pd 
+from helpers.creacionTabla import crearTabla
 
 def analizarCanastaBasica():
-    #2. Sin importar la fuente (sql, xls, JSON, csv..)
-    #Crear tabla tabulada que se le llama DATAFRAME
+    #2. Sin importar la fuente (sql, xls, JSON ,csv...)
+    #Crear una tabla tabulada que se llama DATAFRAME
     tabla=pd.read_csv("./data/bdcanasta.csv")
     #print(tabla)
+    crearTabla(tabla,"canastabasica")
+    
+#3. Aplico filtro para limpiar o seleccionar datos
 
-# Aplico filtros para limpiar o seleccionar datos
-   # print(tabla.head(20)) #Primeros N registros.
-   # print(tabla.tail())
-
-    print(tabla.describe())
+    #print(tabla.head(20)) #Primeros N registros
+    #print(tabla.tail()) #Los ultimos registros   
 
 
